2edd9d3edf568afb50b9d5f2ddefdc23c5b0dc4c
huhua <1535892832@qq.com>
2020-08-28 11:56:22 +0800

    新增app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const DIR_URL = 'C:/Users/15358/Documents/git-study/git-study-master/git-study/';
+
+fs.readdir(DIR_URL, 'utf-8', (err, files) => {
+    if (err) {
+        throw err;
+    }
+    for (furl of files) {
+        let fileUrl = DIR_URL + furl;
+        console.log(fileUrl);
+        fs.readFile(fileUrl, 'utf-8', (err, data) => {
+            if (data != undefined) {
+                console.log(data);
+            }
+        })
+    }
+})
